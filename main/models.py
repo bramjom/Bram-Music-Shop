5ef5e4a42df7cc9ed949b43ba709c508c23f1957
bramjom <ollen331@gmail.com>
2024-09-09 15:56:21 +0700

    Many many commits

---
+++ main/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length = 255)
+    price = models.IntegerField()
+    description = models.TextField()
+    quantity = models.IntegerField()
+"""
+    @property
+    def is_mood_strong(self):
+        return self.mood_intensity > 5
+"""
