9df01ecd745d02ffddc1a42b6da56569e1a17df3
bramjom <ollen331@gmail.com>
2024-09-23 13:04:04 +0700

    Tugas 4

--- main/urls.py
+++ main/urls.py
@@ -1,3 +1,4 @@
+from main.views import register, login_user, logout_user
 from django.urls import path
 from main.views import show_main, create_product,  show_json, show_xml, show_xml_by_id, show_json_by_id
     
@@ -11,4 +12,7 @@ urlpatterns = [
     path('xml/', show_xml, name = "show_xml"),
     path('json/<str:id>/', show_json_by_id, name = "show_json_by_id"),
     path('xml/<str:id>/', show_xml_by_id, name = "show_xml_by_id"),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
     ]
